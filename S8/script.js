77672a21b9b1b66b669ef484c44a85780c4eef86
chouchane.louai20@gmail.com <chouchane.louai20@gmail.com>
2026-03-14 12:15:58 +0100

    javascript-basic-syntax

---
+++ S8/script.js
@@ -0,0 +1,129 @@
+// console.log();
+// var studentName = "Oussama";
+// var studentAge = 20
+// var studentHeight = 1.91
+// var isHungry = true
+// var isFemale = false
+
+var oussama = {
+    studentName: "Oussama",
+    studentAge: 20,
+    studentHeight: 1.91,
+    isHungry: true,
+    isFemale: false,
+    sayHello: () =>{
+        return "My name is "+ oussama.studentName
+    }
+}
+
+
+var soumeya = {
+    studentName: "Soumeya",
+    studentAge: 24,
+    studentHeight: 1.90,
+    isHungry: false,
+    isFemale: true,
+    sayHello: () =>{
+        return "My name is "+ soumeya.studentName
+    }
+}
+
+var racim = {
+    studentName: "Racim",
+    studentAge: 13,
+    studentHeight: 2.99,
+    isHungry: true,
+    isFemale: false,
+    sayHello: () =>{
+        // return "My name is "+ racim.studentName
+        return 0
+    }
+}
+
+
+
+// class Student{
+    //     constructor(name, age, height){}
+// }
+
+// var soumeya2 = new Student("Soumya", 24, 1,90)
+
+
+// console.log(typeof students);
+// console.log(typeof students[2].sayHello());
+
+// Arithmitics
+console.log("Hello " + "There");
+
+// Logical
+// console.log(1+"1" === 11)
+// console.log(7 <= 7);
+
+// var isHungry = true
+// console.log(!isHungry);
+// var age = 18
+// if(age >= 18){
+//     console.log("You are an adult");
+// }else{
+//     console.log("You are a minor");
+// }
+
+var students = [oussama, soumeya, racim]
+// for(let i = 0; i < students.length ; i++){
+//     console.log(students[i].studentName);
+// }
+// var age = 10
+// while(age <= 18){
+//     console.log(age);
+//     age++
+// }
+
+// function displayStudentUpperCase(student){
+    
+//     console.log(student.studentName.toUpperCase());
+
+// }
+
+// for(let i = 0; i < students.length; i++){
+//     displayStudentUpperCase(students[i])
+// }
+
+// function sayHello(name){
+//     console.log("Hello "+name);
+// }
+
+// sayHello("Oussama")
+
+
+// var meal = {
+//     ing: ["", ""]
+// }
+
+// Assignment
+/**
+ * Create a meal object
+ * each meal has a name, prepTime, 3 ingredients
+ * Create a function to display all the meal information
+ * 
+ * create a cookbook that has 3 meals
+ * display all meals
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
