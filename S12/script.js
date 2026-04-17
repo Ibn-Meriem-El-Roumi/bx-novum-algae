3736fca41db9989cc9333550d58408aa4a3ff0c6
chouchane.louai20@gmail.com <chouchane.louai20@gmail.com>
2026-04-17 12:22:14 +0100

    S12, createServer, routing basics

--- S12/script.js
+++ S12/script.js
@@ -3,21 +3,52 @@
 import http from "http"
 import fs from "node:fs"
 import dotenv from "dotenv"
+import { log } from "node:console"
 dotenv.config()
 
 
 const PORT = process.env.PORT || 3000
 
-const server = http.createServer((request, response) =>{
-    fs.readFile("indez.html", (error, data) =>{
+function serveHTMLPage(pageURL, response){
+    let pageName = pageURL.substring(1)
+    if(pageURL === "/") pageName = "index"
+    fs.readFile(`${pageName}.html`, (error, data) =>{
         if(error){
             console.log("Something is wrong");
-            response.writeHead(404, {message: "File not found"})
-            response.end()
+            // fs.readFile("not-found.html", (data) => {
+            //     response.writeHead(404, {message: "File not found"})
+            //     response.end(data)
+            // })
+            // return
         }
         response.writeHead(200, {message: "Wrkd fine"}, {"content-type": "text/html"})
         response.end(data)
     })
+}
+
+
+const server = http.createServer((request, response) =>{
+    console.log(request.method);
+    
+    switch(request.url){
+        case "/":
+            serveHTMLPage(request.url, response)
+            break
+        case "/oussama":
+            serveHTMLPage(request.url, response)
+            break
+        case "/nour":
+            serveHTMLPage(request.url, response)
+            break
+        case "/soumeya":
+            serveHTMLPage(request.url, response)
+            break
+        case "/racim":
+            serveHTMLPage(request.url, response)
+            break
+        default:
+            serveHTMLPage("/notfound", response)
+    }
 })
 
 server.listen(PORT, () =>{
