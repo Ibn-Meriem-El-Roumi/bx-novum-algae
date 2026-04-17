73daca88c0e76808015d91604e84d6b868ff4d1f
chouchane.louai20@gmail.com <chouchane.louai20@gmail.com>
2026-04-17 12:26:57 +0100

    S12 script.js

--- S11/script.js
+++ S11/script.js
@@ -1,3 +1,56 @@
-import fs from "fs"
+/**
+ * Create a CLi
+ * Todo app that manages adding tasks
+ * 
+ * add listAll
+ * Nodejs 
+ */
 
-fs.appendFileSync("file.txt", "\nHello There again \n", "utf-8")
+import {appendFileSync, readFileSync, writeFileSync} from "fs"
+const args = process.argv.splice(2)
+const user = args[1]
+const task = args[2]
+const dueDate = args[3]
+const sourceFile = "todos.tpo"
+switch(args[0]){
+    case "add":
+        addTask(user, task, dueDate)
+        break;
+    case "list":
+        listAll()
+        break;
+    default:
+        console.log(`${args[0]} is not a command: Command not recognised`);
+        break;
+}
+
+function addTask(user, task, dueDate){
+    let newTask = `${user}: ${task} => ${dueDate}\n`
+    appendFileSync(sourceFile, newTask)
+    console.log(`Task added to ${user}`);
+    
+}
+
+function listAll(){
+    const content = readFileSync(sourceFile, {encoding: "utf-8"})
+    const contentArray = content.split("\n")
+    console.log(contentArray);
+    
+    return contentArray
+    // console.log(contentArray);
+    // console.log(typeof content);
+    // console.log("Listing ALL todos");
+}
+
+function deleteList(id){
+    const todos = listAll()
+    const newArray = todos.filter((item, index) => index !== id).filter(item => item !== "")
+    console.log(newArray);
+    writeFileSync(sourceFile, "", {encoding: "utf-8"})
+    newArray.forEach(item => {
+        appendFileSync(sourceFile, `${item}\n`)
+        console.log(item);
+    })
+}
+
+deleteList(2)
